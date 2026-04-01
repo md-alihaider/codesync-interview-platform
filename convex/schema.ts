a280292b42827d8bb55068e1b6515847c781aee7
Md Ali Haider <alihaiderbgp85@gmail.com>
2026-04-02 00:03:16 +0530

    feat: define interview and comments tables in schema

--- convex/schema.ts
+++ convex/schema.ts
@@ -6,8 +6,27 @@ export default defineSchema({
     name: v.string(),
     email: v.string(),
     image: v.optional(v.string()),
-    role: v.union(v.literal("candidate"), v.literal("interviewer")), // "candidate" or "interviewer"
+    role: v.union(v.literal("candidate"), v.literal("interviewer")),
     clerkId: v.string(),
-  }).index("by_clerkId", ["clerkId"]),
-});
+  }).index("by_clerk_id", ["clerkId"]),
 
+  interviews: defineTable({
+    title: v.string(),
+    description: v.optional(v.string()),
+    startTime: v.number(),
+    endTime: v.optional(v.number()),
+    status: v.string(),
+    streamCallId: v.string(),
+    candidateId: v.string(),
+    interviewerIds: v.array(v.string()),
+  })
+    .index("by_candidate_id", ["candidateId"])
+    .index("by_stream_call_id", ["streamCallId"]),
+
+  comments: defineTable({
+    content: v.string(),
+    rating: v.number(),
+    interviewerId: v.string(),
+    interviewId: v.id("interviews"),
+  }).index("by_interview_id", ["interviewId"]),
+});
