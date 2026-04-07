c716f40fff67e2bf3e844dfbac3db2a9a996383e
Md Ali Haider <alihaiderbgp85@gmail.com>
2026-04-07 12:05:19 +0530

    feat: implement Home Page UI components and Stream video provider

--- app/page.tsx
+++
@@ -1,8 +0,0 @@
-import { Button } from "@/components/ui/button";
-import { Show, SignInButton, SignUpButton, UserButton } from "@clerk/nextjs";
-
-export default function Home() {
-  return <div className="m-10">
-    Home page 
-  </div>;
-}
